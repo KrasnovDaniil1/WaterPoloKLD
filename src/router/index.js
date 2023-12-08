d65c389cad43647c4d5ea6b8f346fe63f3bf4144
KrasnovDaniil1 <kd69931@gmail.com>
2023-12-08 21:42:05 +0200

    add header

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,48 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [],
+    routes: [
+        {
+            path: "/",
+            name: "main",
+            component: () => import("../views/Main.vue"),
+        },
+        {
+            path: "/amateurs",
+            name: "amateurs",
+            component: () => import("../views/Amateurs.vue"),
+        },
+        {
+            path: "/children",
+            name: "children",
+            component: () => import("../views/Children.vue"),
+        },
+        {
+            path: "/students",
+            name: "students",
+            component: () => import("../views/Students.vue"),
+        },
+        {
+            path: "/federation",
+            name: "federation",
+            component: () => import("../views/Federation.vue"),
+        },
+        {
+            path: "/news",
+            name: "news",
+            component: () => import("../views/News.vue"),
+        },
+        {
+            path: "/news/:id",
+            name: "news_id",
+            component: () => import("../views/NewsID.vue"),
+        },
+        {
+            path: "/contacts",
+            name: "contacts",
+            component: () => import("../views/Contacts.vue"),
+        },
+    ],
 });
 
 export default router;
---
+++ src/views/NewsID.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
