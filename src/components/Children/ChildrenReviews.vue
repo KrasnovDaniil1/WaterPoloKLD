fca5f6d1a96a3dffcc521c0325265953527956b9
KrasnovDaniil1 <kd69931@gmail.com>
2023-12-20 22:41:56 +0200

    add

--- src/components/Children/ChildrenReviews.vue
+++ src/components/Children/ChildrenReviews.vue
@@ -8,51 +8,69 @@ export default {
     setup() {
         const activeElem = ref(1);
         const cardBlock = ref();
-        const reviews = [
+        const reviews = ref([
             {
-                text: ", tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
+                text: "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
                 name: "Имя Отчеств",
             },
             {
-                text: " consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
+                text: "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
                 name: "Имя Отчест",
             },
             {
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
+                text: "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
                 name: "Имя Отчес",
             },
+
             {
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. Vestibulum consequat erat sapien, a pellentesque quam accumsan nec.",
-                name: "Имя Отче",
+                text: "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. ",
+                name: "Имя -2",
             },
             {
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. ",
-                name: "Имя Отч",
+                text: "5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. ",
+                name: "Имя -1",
             },
-            {
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. ",
-                name: "Имя От",
-            },
-            {
-                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus suscipit metus sit amet nisi lacinia condimentum. Phasellus eu nibh lobortis, tincidunt nisi in, ornare leo. Donec hendrerit interdum mollis. ",
-                name: "Имя О",
-            },
-        ];
+        ]);
         const widthBlock = ref(1600);
         const scrolAdaptive = ref(0);
         const transformTranslate = ref(0);
+        const windowWidth = ref(window.innerWidth);
 
         const scrolCard = (index) => {
-            console.log(activeElem.value, index);
             if (activeElem.value - 1 == index) {
-                activeElem.value -= 1;
+                scrolLeft();
             } else if (activeElem.value == index) {
-                activeElem.value += 1;
                 scrolRight();
             }
         };
 
+        // const scrolRight = () => {
+        //     if (reviews.length >= activeElem.value + 3) {
+        //         activeElem.value += 1;
+        //         cardBlock.value.style.transform = `translateX(-${
+        //             transformTranslate.value * (activeElem.value - 1) +
+        //             scrolAdaptive.value
+        //         }px)`;
+        //     } else {
+        //         activeElem.value = 1;
+        //         cardBlock.value.style.transform = `translateX(0px)`;
+        //     }
+        // };
         const scrolRight = () => {
+            activeElem.value += 1;
+            if (activeElem.value >= reviews.value.length - 1) {
+                activeElem.value = 1;
+            }
+            cardBlock.value.style.transform = `translateX(-${
+                transformTranslate.value * (activeElem.value - 1) +
+                scrolAdaptive.value
+            }px)`;
+        };
+        const scrolLeft = () => {
+            activeElem.value -= 1;
+            if (activeElem.value < 1) {
+                activeElem.value = reviews.value.length - 2;
+            }
             cardBlock.value.style.transform = `translateX(-${
                 transformTranslate.value * (activeElem.value - 1) +
                 scrolAdaptive.value
@@ -60,8 +78,17 @@ export default {
         };
 
         onMounted(() => {
+            reviews.value.push(reviews.value[reviews.value.length - 1]);
+            reviews.value.unshift(reviews.value[0]);
+
             transformTranslate.value = cardBlock.value.children[0].clientWidth;
-            if (widthBlock.value > window.innerWidth) {
+            if (window.innerWidth <= 834) {
+                widthBlock.value = 801;
+            }
+            if (window.innerWidth <= 390) {
+                widthBlock.value = 480;
+            }
+            if (widthBlock.value >= window.innerWidth) {
                 scrolAdaptive.value =
                     (widthBlock.value - window.innerWidth) / 2;
                 cardBlock.value.style.transform = `translateX(-${scrolAdaptive.value}px)`;
@@ -76,6 +103,8 @@ export default {
             widthBlock,
             transformTranslate,
             scrolAdaptive,
+            scrolLeft,
+            windowWidth,
         };
     },
 };
@@ -98,14 +127,15 @@ export default {
                         </p>
                         <h3 class="content__name">{{ item.name }}</h3>
                     </div>
-                    <Icons
-                        icons="arrow"
-                        class="card__arrow"
+                    <div
+                        class="card__arrow__block"
                         :class="{
                             card__arrow__previews: index == activeElem - 1,
                         }"
                         @click="scrolCard(index)"
-                    />
+                    >
+                        <Icons icons="arrow" class="card__arrow" />
+                    </div>
                 </nav>
             </div>
         </nav>
@@ -140,14 +170,14 @@ export default {
                 width: fit-content;
                 display: flex;
                 align-items: center;
-                .card__arrow {
+                .card__arrow__block {
                     min-width: 222px;
                     transition: opacity 0.25s;
                     opacity: 0;
-                    transform: translateX(-15%) rotate(180deg);
-                    svg {
+                    transform: translateX(-50%) rotate(180deg);
+                    .card__arrow {
                         width: clamp(44px, calc(66vw / var(--ratio)), 66px);
-                        height: clamp(44px, calc(6vw / var(--ratio)), 66px);
+                        height: clamp(44px, calc(66vw / var(--ratio)), 66px);
                     }
                 }
                 .card__arrow__previews {
@@ -204,9 +234,9 @@ export default {
                 }
             }
             .card__active {
-                .card__arrow {
-                    transform: translateX(15%) rotate(0deg);
+                .card__arrow__block {
                     opacity: 0.4;
+                    transform: translateX(50%) rotate(0deg);
                     &:hover {
                         opacity: 1;
                     }
@@ -214,6 +244,123 @@ export default {
                 .card__content {
                     opacity: 1;
                     transform: scale(1.35);
+                }
+            }
+        }
+    }
+}
+@media screen and (max-width: 834px) {
+    .reviews {
+        max-width: 100%;
+        .reviews__title {
+            font-size: clamp(36px, calc(64vw / var(--ratio)), 64px);
+            letter-spacing: 1.28px;
+        }
+
+        .reviews__block {
+            padding: 90px 0;
+
+            // transform: translateX(-35px);
+            .block__card {
+                .card {
+                    .card__arrow__block {
+                        // min-width: 222px
+                        min-width: 100px;
+                        transform: translateX(-65%) rotate(180deg);
+                        .card__arrow {
+                            width: clamp(24px, calc(30vw / var(--ratio)), 30px);
+                            height: clamp(
+                                24px,
+                                calc(44vw / var(--ratio)),
+                                44px
+                            );
+                        }
+                    }
+
+                    .card__content {
+                        min-width: 195px;
+                        padding: clamp(8px, calc(16vw / var(--ratio)), 16px);
+                        border-radius: 16px;
+
+                        .content__icon {
+                            width: 16px;
+                            height: 16px;
+                        }
+                        .content__text {
+                            margin-top: 16px;
+                            font-size: 8px;
+                            letter-spacing: 0.64px;
+                        }
+                        .content__name {
+                            padding-top: 16px;
+                            font-size: 12px;
+                            letter-spacing: 0.24px;
+                        }
+                    }
+                }
+                .card__active {
+                    .card__arrow__block {
+                        transform: translateX(65%) rotate(0deg);
+                    }
+                    .card__content {
+                        transform: scale(1.5);
+                    }
+                }
+            }
+        }
+    }
+}
+@media screen and (max-width: 390px) {
+    .reviews {
+        .reviews__title {
+            font-size: clamp(24px, calc(36vw / var(--ratio)), 36px);
+            letter-spacing: 0.72px;
+        }
+
+        .reviews__block {
+            padding: 60px 0 40px 0;
+
+            // transform: translateX(-35px);
+            .block__card {
+                .card {
+                    .card__arrow__block {
+                        // min-width: 222px
+                        min-width: 70px;
+                        transform: translateX(-55%) rotate(180deg);
+                        .card__arrow {
+                            width: 24px;
+                            height: 24px;
+                        }
+                    }
+
+                    .card__content {
+                        min-width: 107px;
+                        padding: 8px;
+                        border-radius: 8px;
+
+                        .content__icon {
+                            width: 8px;
+                            height: 8px;
+                        }
+                        .content__text {
+                            margin-top: 8px;
+                            font-size: 6px;
+                            letter-spacing: 0.24px;
+                        }
+                        .content__name {
+                            padding-top: 10px;
+                            font-size: 6px;
+                            letter-spacing: 0.12px;
+                        }
+                    }
+                }
+                .card__active {
+                    .card__arrow__block {
+                        transform: translateX(55%) rotate(0deg);
+                    }
+                    .card__content {
+                        transform: scale(1.5);
+                    }
                 }
             }
         }
